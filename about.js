41e3a543ea356e9692344b35b4c2f039a1f5c3fa
agboola vincent <agboolavincent67@gmail.com>
2024-04-09 12:16:43 +0100

    earnfest

---
+++ about.js
@@ -0,0 +1,10 @@
+// JavaScript for menu toggle
+document.getElementById('menuBtn').addEventListener('click', function() {
+    document.getElementById('menu').style.display = 'block';
+});
+
+document.getElementById('closeBtn').addEventListener('click', function() {
+    document.getElementById('menu').style.display = 'none';
+});
+
+
